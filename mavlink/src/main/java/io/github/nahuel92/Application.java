47d696dcb767df6e4517f80dc335cb0ad4a6545a
Nahuel Rodriguez <forte_nrodriguez@arbormetrix.com>
2025-01-23 10:18:07 -0300

    chore: adding new Micronaut project

--- mavlink/src/main/java/io/github/nahuel92/Application.java
+++
@@ -1,9 +0,0 @@
-package io.github.nahuel92;
-
-import io.micronaut.runtime.Micronaut;
-
-public class Application {
-    public static void main(final String... args) {
-        Micronaut.run(Application.class, args);
-    }
-}
