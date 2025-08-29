274676b256651343b4fb623ad308482cc4847e58
Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>
2025-08-29 16:35:41 -0300

    feat(micronaut): Added new project
    Signed-off-by: Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>

--- micronaut/micronaut-data-batch-bug/src/main/java/io/github/nahuel92/MyEntity.java
+++ micronaut/micronaut-data-batch-bug/src/main/java/io/github/nahuel92/MyEntity.java
@@ -5,14 +5,10 @@ import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.data.annotation.MappedProperty;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @MappedEntity(value = "my_entity")
 public record MyEntity(
         @MappedProperty("id") @Id @GeneratedValue @Nullable Integer id,
-        @MappedProperty("field_1") @NotBlank String field1,
-        @MappedProperty("field_2") @NotNull @Min(0) @Max(2) Integer field2) {
+        @MappedProperty("field_1") String field1,
+        @MappedProperty("field_2") Integer field2) {
 }
